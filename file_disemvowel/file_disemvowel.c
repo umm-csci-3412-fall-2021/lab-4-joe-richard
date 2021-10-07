9987f3a30820671dc857aba1fd13dbd10654eec4
Richard Lussier <lussi036@morris.umn.edu>
2021-10-07 10:37:30 -0500

    Implement file_disemvowel.c for stdin and stdout
    Created and implemented the file_disemvowel.c file. The file currently only works with stdin and stdout.
    
    Co-authored-by: helloworld12321 <walbr037@morris.umn.edu>

---
+++ file_disemvowel/file_disemvowel.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+#define BUF_SIZE 1024
+
+bool is_vowel(char c) {
+	char lowercase = tolower(c);
+	if(lowercase == 'a' || lowercase == 'e' || lowercase == 'i'
+			|| lowercase == 'o' || lowercase == 'u') {
+		return true;
+	}
+	return false;
+}
+
+/*
+ * Copy all the non-vowels from in_buf to out_buf.
+ * num_chars indicates how many characters are in in_buf,
+ * and this function should return the number of non-vowels that
+ * that were copied over.
+ *
+ * As a precondition, out_buf should be at least as large as num_chars.
+ */
+int copy_non_vowels(int num_chars, char* in_buf, char* out_buf) {
+	int chars_copied = 0;
+	for (int i = 0; i < num_chars; i++) {
+		if (!is_vowel(in_buf[i])) {
+			out_buf[chars_copied] = in_buf[i];
+			chars_copied++;
+		}
+	}
+	return chars_copied;
+}
+
+/*
+ * Copy all the non-vowels from inputFile to outputFile.
+ * Create input and output buffers, and use fread() to repeatedly read
+ * in a buffer of data, copy the non-vowels to the output buffer, and
+ * use fwrite to write that out.
+ */
+void disemvowel(FILE* inputFile, FILE* outputFile) {
+	char in_buf[BUF_SIZE];
+	char out_buf[BUF_SIZE];
+	while(!feof(inputFile)) {
+		int num_chars_read = fread(in_buf, sizeof(char), BUF_SIZE, inputFile);
+		int non_vowel_count = copy_non_vowels(num_chars_read, in_buf, out_buf);
+		fwrite(out_buf, sizeof(char), non_vowel_count, outputFile);
+	}
+}
+
+int main(int argc, char *argv[]) {
+    // This sets these to `stdin` and `stdout` by default.
+    // You then need to set them to user specified files when the user
+    // provides files names as command line arguments.
+    FILE *inputFile = stdin;
+    FILE *outputFile = stdout;
+
+    // Code that processes the command line arguments
+    // and sets up inputFile and outputFile.
+
+    disemvowel(inputFile, outputFile);
+
+    return 0;
+}
+
