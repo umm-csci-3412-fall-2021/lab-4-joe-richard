1b0e06431336d7646c0ad8901a8188675cf8a6f4
Richard Lussier <lussi036@morris.umn.edu>
2021-10-12 11:13:46 -0500

    Implement summarize_tree.c
    Implemented summarize_tree.c, which takes a path name and will output the number of directories and files that path contains.
    
    Co-authored-by: helloworld12321 <walbr037@morris.umn.edu>

--- summarize_tree/summarize_tree.c
+++ summarize_tree/summarize_tree.c
@@ -5,44 +5,55 @@
 #include <dirent.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
 
 static int num_dirs, num_regular;
 
+//checks whether the given path is a directory
 bool is_dir(const char* path) {
-  /*
-   * Use the stat() function (try "man 2 stat") to determine if the file
-   * referenced by path is a directory or not.  Call stat, and then use
-   * S_ISDIR to see if the file is a directory. Make sure you check the
-   * return value from stat() in case there is a problem, e.g., maybe the
-   * the file doesn't actually exist.
-   */
+	struct stat buf;
+	int stat_error = stat(path, &buf);
+
+	if(stat_error == 0) {
+		return S_ISDIR(buf.st_mode);
+	}
+	fprintf(stderr, "We were unable to stat the path provided.\n");
+	fprintf(stderr, "Path Name: %s\n", path);
+	exit(1);
 }
 
-/* 
- * I needed this because the multiple recursion means there's no way to
- * order them so that the definitions all precede the cause.
- */
 void process_path(const char*);
 
 void process_directory(const char* path) {
-  /*
-   * Update the number of directories seen, use opendir() to open the
-   * directory, and then use readdir() to loop through the entries
-   * and process them. You have to be careful not to process the
-   * "." and ".." directory entries, or you'll end up spinning in
-   * (infinite) loops. Also make sure you closedir() when you're done.
-   *
-   * You'll also want to use chdir() to move into this new directory,
-   * with a matching call to chdir() to move back out of it when you're
-   * done.
-   */
+	num_dirs++;
+
+	int chdir_error = chdir(path);
+	if(chdir_error != 0) {
+		perror("Cannot chdir into the path\n");
+		fprintf(stderr, "Path name: %s\n", path);
+		exit(1);
+	}
+
+	DIR *dir = opendir(".");
+	struct dirent *child;
+	//look at every file in the directory and recursively call process_path
+	while((child = readdir(dir)) != NULL) {
+		if(strcmp(child->d_name, "..") == 0 || strcmp(child->d_name, ".") == 0) {
+			continue;
+		}
+		process_path(child->d_name);
+	}
+	closedir(dir);
+
+	int end_chdir_error = chdir("..");
+	if(end_chdir_error != 0) {
+		fprintf(stderr, "Cannot go back into the previous directory from %s\n", path);
+		exit(1);
+	}
 }
 
 void process_file(const char* path) {
-  /*
-   * Update the number of regular files.
-   * This is as simple as it seems. :-)
-   */
+	num_regular++;
 }
 
 void process_path(const char* path) {
